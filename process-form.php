6faa66a5090cf4ad75d65733cecadb710cd1f3a6
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-27 02:56:38 +0530

    php

--- process-form.php
+++ process-form.php
@@ -1,14 +1,21 @@
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$to = 'satwikdpshrit@gmail.com';
-$subject = 'New Message From Your Website';
-$body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: $email\r\n";
-$headers .= "Reply-To: $email\r\n";
-if (mail($to, $subject, $body, $headers)) {
-  echo 'Thank you for your message!';
-} else {
-  echo 'An error occurred. Please try again later.';
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get form input values
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    // Email details
+    $to = 'satwikdpshrit@gmail.com';
+    $subject = 'New Contact Message';
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage:\n$message";
+
+    // Send email
+    if(mail($to, $subject, $body)) {
+        echo 'Message sent successfully.';
+    } else {
+        echo 'Error sending message.';
+    }
 }
+?>
